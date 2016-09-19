3cdf532bfcb9122930ee3599e21afbdaa1b0dd4f
Mike Haydon <mike.c.haydon@gmail.com>
2016-09-19 13:43:06 -0500

    Added Comments.

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -15,7 +15,14 @@ public class Startup {
         Course p2 = new IntroJavaCourse("Intro to Java", "152-134", 4, "Intro to Programming");
         Course p3 = new AdvancedJavaCourse("Advanced Java", "152-135", 4, "Intro to Java");
         
-        System.out.println(p3.getCredits());
-        System.out.println(p1.getCredits());
+        System.out.printf("Courses\t\t\tClass Number\tCredits\n--------------------\t------------\t-------\n");
+        System.out.printf("%s\t%s\t\t%f\n", p1.getCourseName(), p1.getCourseNumber(), p1.getCredits());
+        System.out.printf("%s\t\t%s\t\t%f\n", p2.getCourseName(), p2.getCourseNumber(), p2.getCredits());
+        System.out.printf("%s\t\t%s\t\t%f\n", p3.getCourseName(), p2.getCourseNumber(), p3.getCredits());
+        
     }
 }
+
+//This method words well if you want all objects to have the same properties and methods.
+//However, I do not like that I can not call the methods that just the classes have when calling them by their 
+//parent class datatype.
--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -15,7 +15,15 @@ public class Startup {
         Course p2 = new IntroJavaCourse("Intro to Java", "152-134");
         Course p3 = new AdvancedJavaCourse("Advanced Java", "152-135");
         
-        System.out.println(p3.getCourseName());
-        System.out.println(p1.getCredits());
+        System.out.printf("Courses\t\t\tClass Number\tCredits\n--------------------\t------------\t-------\n");
+        System.out.printf("%s\t%s\t\t%f\n", p1.getCourseName(), p1.getCourseNumber(), p1.getCredits());
+        System.out.printf("%s\t\t%s\t\t%f\n", p2.getCourseName(), p2.getCourseNumber(), p2.getCredits());
+        System.out.printf("%s\t\t%s\t\t%f\n", p3.getCourseName(), p2.getCourseNumber(), p3.getCredits());
+        
     }
 }
+
+//This method appears to be a bit easier to work with. Nothing is implied so there is no trying to remember
+//which classes had which things inherited automatically. 
+//However I am again unable to call for instance p3.getPrerequistes. Is there any way to call these when using
+//the parent datatype that does not have these methods defined?
